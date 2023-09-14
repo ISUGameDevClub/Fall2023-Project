4f4ea5c4f08a71d8e16d3ccd69861187da49ea6f
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2023-09-13 22:04:49 -0500

    Update PlayerMovement.cs

--- Assets/Programming/Scripts/PlayerMovement.cs
+++ Assets/Programming/Scripts/PlayerMovement.cs
@@ -6,13 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     Transform playerTransform;
+
     // Update is called once per frame
     void Update()
     {
         playerTransform = transform;
+        LeftRightMove();
+    }
+    void LeftRightMove()
+    {
         if (Input.GetAxis("Horizontal") != 0)
         {
-            transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal") * speed,transform.position.y);
+            transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal") * speed, transform.position.y);
         }
     }
 }
