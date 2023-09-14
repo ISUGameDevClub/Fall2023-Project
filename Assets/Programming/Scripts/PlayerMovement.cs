f7bfee966f371d32f6ff692d2e45e9c5b66d5c3f
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2023-09-13 22:00:15 -0500

    tutorial stuff

---
+++ Assets/Programming/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Transform playerTransform;
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform = transform;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal") * speed,transform.position.y);
+        }
+    }
+}
