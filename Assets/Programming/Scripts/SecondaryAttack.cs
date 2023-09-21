c63cdc27ba77fbd5cdf73ba2b32e50e5a0d7bc05
brycejodison <brycejodison2587@gmail.com>
2023-09-21 00:33:34 -0500

    Implemented system that checks for what weapon state player is in and executes code depending on it.
    Checks weapon state and does things depending on which one player is in. Added bool that can later be changed to determine if the player is in capable of performing secondary attack. Added coroutine that executes when player presses right mouse click and is capable of performing a secondary attack. Added basic variables that can be altered to change instantiated projectiles appearance and speed via the inspector. Added basic script that destroys bullets after a set amount of time. (No collision detection yet)

--- Assets/Programming/Scripts/SecondaryAttack.cs
+++ Assets/Programming/Scripts/SecondaryAttack.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SecondaryAttack : MonoBehaviour
 {
-    public GameObject placeholder;
-    
+    [Header("Attack Objects")]
+    public GameObject secondaryAttackOne;
+    public GameObject secondaryAttackTwo;
+    public GameObject secondaryAttackThree;
+
+    [Header("Projectile Speeds")]
+    public float objectOneTravelSpeed;
+    public float objectTwoTravelSpeed;
+    public float objectThreeTravelSpeed;
+
+    [Header("Attack Information")]
+    //Set to false when unable to use secondary attack (out of energy/currency)
+    public bool canUseSecondaryAttack = true;
+    public int weaponState = 0;
     
     void Update()
     {
@@ -14,9 +28,38 @@ public class SecondaryAttack : MonoBehaviour
 
     void CheckForClick()
     {
-       if(Input.GetKeyDown(KeyCode.Mouse1))
-       {
-            Instantiate(placeholder,transform);
-       }
+        if(weaponState == 0)
+        {
+            return;
+        }
+        else if(weaponState == 1)
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse1) && canUseSecondaryAttack)
+            {
+                StartCoroutine(SecondaryOne());
+                Debug.Log("Secondary Attack 1 Pressed");
+            }
+        }
+        else if(weaponState == 2)
+        {
+            //insert second secondary weapon state functionality
+        }
+        else if(weaponState == 3)
+        {
+            //insert third secondary weapon state functionality
+        }
+        
+        
     }
+
+    //Note that secondary attacks do not need to be projectiles. All of this is placeholder until we have solid ideas
+    private IEnumerator SecondaryOne()
+    {
+        //Instantiates object. Can add other functionality upon request. Will currently move object forward along x axis at designated speed
+        GameObject clone;
+        clone = Instantiate(secondaryAttackOne, transform.position + new Vector3(1,0,0), transform.rotation); //will set transform to players weapon when model is implemented
+        clone.GetComponent<Rigidbody2D>().velocity = new Vector3(objectOneTravelSpeed,0,0);
+        yield return new WaitForSeconds(.1f);
+    }
+
 }
