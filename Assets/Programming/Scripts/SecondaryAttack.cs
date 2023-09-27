8e2ad6a5a071a09474abbf58f4b8093fa27f7bf3
brycejodison <brycejodison2587@gmail.com>
2023-09-27 12:31:45 -0500

    Added more functionality for melee
    Can now attack upwards if holding W and left clicking. Added more inspector functionality for level designers

--- Assets/Programming/Scripts/SecondaryAttack.cs
+++ Assets/Programming/Scripts/SecondaryAttack.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class SecondaryAttack : MonoBehaviour
 {
+    [Header("Player Info")]
+    public SpriteRenderer spriteRenderer;
+
     [Header("Attack Objects")]
     public GameObject secondaryAttackOne;
-    public GameObject secondaryAttackTwo;
+    public GameObject secondaryAttackTwoR;
+    public GameObject secondaryAttackTwoL;
+    public GameObject secondaryAttackTwoU;
+
     public GameObject secondaryAttackThree;
 
     [Header("Projectile Speeds")]
@@ -20,10 +27,17 @@ public class SecondaryAttack : MonoBehaviour
     public int weaponState = 0;
     //Alter this variable to change amount of time hitbox is active on attack two
     public float attackTwoDestroyTime = .2f;
+    public float attackTwoCooldown = 2f;
+    public bool debounce;
+    
     
     void Start()
     {
-        secondaryAttackTwo.SetActive(false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        secondaryAttackTwoR.SetActive(false);
+        secondaryAttackTwoL.SetActive(false);
+        secondaryAttackTwoU.SetActive(false);
     }
     void Update()
     {
@@ -36,22 +50,32 @@ public class SecondaryAttack : MonoBehaviour
         {
             return;
         }
+        //projectile
         else if(weaponState == 1)
         {
             if(Input.GetKeyDown(KeyCode.Mouse1) && canUseSecondaryAttack)
             {
                 StartCoroutine(SecondaryOne());
+                //insert method that subtracts from currency
                 Debug.Log("Secondary Attack 1 Pressed");
             }
         }
-        else if(weaponState == 2)
+        //melee
+        else if(weaponState == 2) 
         {
             if(Input.GetKeyDown(KeyCode.Mouse1) && canUseSecondaryAttack)
             {
-                StartCoroutine(SecondaryTwo());
-                Debug.Log("Secondary Attack 2 Pressed");
+                if(!debounce)
+                {
+                    debounce = true;
+                    StartCoroutine(SecondaryTwo());
+                    //insert method that subtracts from currency
+                    Debug.Log("Secondary Attack 2 Pressed");
+                }
+                
             }
         }
+        //to be determined
         else if(weaponState == 3)
         {
             //insert third secondary weapon state functionality
@@ -70,13 +94,33 @@ public class SecondaryAttack : MonoBehaviour
         yield return new WaitForSeconds(.1f);
     }
 
-    //Instantiate a temporary collider (Can make a permanent object that can be enabled/disabled when button is clicked instead(will allow hitbox to follow player))
+    //Enables then disables collider
     //collider is not visible in game view
     private IEnumerator SecondaryTwo()
     {
-        secondaryAttackTwo.SetActive(true);
-        yield return new WaitForSeconds(attackTwoDestroyTime);
-        secondaryAttackTwo.SetActive(false);
+        if(Input.GetKey(KeyCode.W))
+        {
+            secondaryAttackTwoU.SetActive(true);
+            yield return new WaitForSeconds(attackTwoDestroyTime);
+            secondaryAttackTwoU.SetActive(false);
+        }
+        else if(spriteRenderer.flipX == false)
+        {
+            secondaryAttackTwoR.SetActive(true);
+            yield return new WaitForSeconds(attackTwoDestroyTime);
+            secondaryAttackTwoR.SetActive(false);
+        }
+        //Check direction player is facing(will only attack right because no code exists to flip player)
+        else if(spriteRenderer.flipX == true)
+        {
+            secondaryAttackTwoL.SetActive(true);
+            yield return new WaitForSeconds(attackTwoDestroyTime);
+            secondaryAttackTwoL.SetActive(false);
+        }
+        
+        yield return new WaitForSeconds(attackTwoCooldown);
+        debounce = false;
+        
     }
 
 }
