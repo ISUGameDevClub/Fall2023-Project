0f3568543038590865fed9213df49a76a13028a3
Shanes444 <shane.stevens444@gmail.com>
2023-09-27 21:51:09 -0500

    Created epic pause menu

--- Assets/Programming/Scripts/PauseScreen.cs
+++ Assets/Programming/Scripts/PauseScreen.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScreen : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
-    public GameObject pauseMenu; 
+    public GameObject pauseMenu;
+    public GameObject pause;
+    public GameObject options;
 
     // Update is called once per frame
     void Update()
@@ -25,15 +28,32 @@ public class PauseScreen : MonoBehaviour
         }
     }
 
-    void Resume()
+    public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        GameIsPaused = false;
+        pause.SetActive(true);
+        options.SetActive(false);
     }
 
     void Pause()
     {
         pauseMenu.SetActive(true);
+        //ZA WARUDO!!!
         Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("NOOOOO! PLEASE DON'T GO!!! I DON'T KNOW WHAT I'LL DO WITHOUT YOU!!!");
+        SceneManager.LoadScene("MainMenuTest");
+    }
+
+    public void OptionsMenu()
+    {
+        pause.SetActive(false);
+        options.SetActive(true);
     }
 }
