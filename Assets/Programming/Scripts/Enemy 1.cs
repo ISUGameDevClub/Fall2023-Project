968841ddf28b47df28665e947a00b09709203a0c
DaneIwema <daneiwema@gmail.com>
2023-09-27 21:54:14 -0500

    Work on Enemy 1

---
+++ Assets/Programming/Scripts/Enemy 1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour{
+
+    [SerializeField] private int health;
+    
+    [SerializeField] private double moveSpeed;
+
+    //true is forward, false is backwards
+    private bool direction;
+    SpriteRenderer enemyRenderer;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    //called to subtract enemy health and blink out for a frame to signify being hit
+    public void damage(int damageTaken){
+        health = health - damageTaken;
+        enemyRenderer = GetComponent<SpriteRenderer>();
+        //Change the GameObject's Material Color to white
+        enemyRenderer.enabled = false;
+        yield return new WaitForSeconds(1);
+        enemyRenderer.enabled = true;
+    }
+
+    //detects collison with anything but the player and reverses the movement
+    private void OnCollisionEnter2D(Collision2D other){
+        if (other.gameObject.tag != "Player" && direction){
+            direction = false;
+            enemyRenderer.flipX = true;
+        }
+        else if (other.gameObject.tag != "Player" && !direction){
+            direction = true;
+            enemyRenderer.flipX = false;
+        }
+    }
+}
