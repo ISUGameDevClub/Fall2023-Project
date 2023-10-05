9ed738834abe2aeb4344db2dbfc7ee15606d5819
DaneIwema <daneiwema@gmail.com>
2023-10-04 21:57:30 -0500

    Enemy 1 working back and forth collision

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour{
+
+    [SerializeField] private int health;
+    
+    [SerializeField] private int moveSpeed;
+
+    private SpriteRenderer sprite;
+
+    //true is forward, false is backwards
+    private bool direction;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        flicker();
+        if (direction){
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        else {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        if (health < 0){
+            Destroy(this);
+        }
+    }
+
+    //detects collison with anything but the player and reverses the movement
+    private void OnCollisionEnter2D(Collision2D other){
+        if (other.gameObject.tag != "Player" && direction){
+            direction = false;
+        }
+        else if (other.gameObject.tag != "Player" && !direction){
+            direction = true;
+        }
+    }
+
+    public void TakeDamage(int damageTaken){
+        health = health - damageTaken;
+        
+    }
+
+    private void flicker(){
+        sprite.color = new Color(1f,1f,1f,0f);
+        sprite.color = new Color(1f,1f,1f,100f);
+    }
+}
