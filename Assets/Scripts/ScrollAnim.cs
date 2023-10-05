720c991eeaed205c26fb9955a8b0cb1df8247ccb
almacle <43427079+thewolfpack23x@users.noreply.github.com>
2023-10-04 21:27:14 -0500

    Added transition from credits to title screen

--- Assets/Scripts/ScrollAnim.cs
+++ Assets/Scripts/ScrollAnim.cs
@@ -7,8 +7,12 @@ public class ScrollAnim : MonoBehaviour
     Transform scrollTransform;
     [SerializeField] float scrollSpeed;
     [SerializeField] float stopPoint;
+    [SerializeField] float endTimer;
     public bool creditsComplete;
+
     [SerializeField] string yPosition; //for debug purposes
+    public LevelLoader LoadScript;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +30,18 @@ public class ScrollAnim : MonoBehaviour
         else
         {
             creditsComplete = true;
+        }
+        
+        if (creditsComplete == true)
+        {
+            if (endTimer > 0)
+            {
+                endTimer -= Time.deltaTime;
+            }
+            else
+            {
+                LoadScript.LoadNextLevel();
+            }
         }
     }
 }
