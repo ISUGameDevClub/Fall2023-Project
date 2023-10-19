86452a79f91036ffab79d8affce08038b134314f
DaneIwema <daneiwema@gmail.com>
2023-10-18 21:53:31 -0500

    EnemyController Updates, changed movement physics, added serialezed filed for enemy type controlling, also added srpite flipping when directions change

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,47 +1,81 @@
+using System;
 using UnityEngine;
 
 
 public class EnemyController : MonoBehaviour{
-    //Goomba enemy should walk through the player and we should be able to use this script for all enemies. we can create a string that has a dropdown of values in inspector that changes what type of enemy it is. Can also be an int if we want to do that for testing. lmk if you have questions -c
+    //Goomba enemy should walk through the player and we should be able to use this script for all enemies.
     [SerializeField] private int health;
     
     [SerializeField] private int moveSpeed;
 
     [SerializeField] SpriteRenderer sprite;
 
-    Animator animator;
+    Rigidbody2D rb;
 
-    //true is forward, false is backwards
-    private bool direction;
-    // Start is called before the first frame update
-    void Start(){
+    [SerializeField] enemySelection es;
+    private Boolean direction;
+    public Boolean getDirection(){
+        return direction;
     }
 
-    // Update is called once per frame
+    private enum enemySelection {
+        enemy1,
+        enemy2,
+        enemy3
+    };
+
+    Animator animator;
+    Physics2D physics;
+
+    void Start(){
+        direction = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update(){
-        if (direction){
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-        }
-        else {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-        }
-        if (health < 0){
-            Destroy(this);
+        switch (es) {
+            case enemySelection.enemy1:{
+                updateEnemy1();
+                break;
+            }
+            case enemySelection.enemy2:{
+
+                break;
+            }
+            case enemySelection.enemy3:{
+
+                break;
+            }
         }
     }
 
     //detects collison with anything but the player and reverses the movement
     private void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.tag != "Player" && direction){
+        if (other.gameObject.tag == "Wall" && direction) {
             direction = false;
         }
-        else if (other.gameObject.tag != "Player" && !direction){
+        else if (other.gameObject.tag == "Wall" && !direction){
             direction = true;
         }
     }
 
+    private void updateEnemy1(){
+        if (direction){
+            rb.AddForce(Vector2.right * moveSpeed);
+            this.sprite.flipY = false;
+        }
+        else{
+            rb.AddForce(Vector2.left * moveSpeed);
+            this.sprite.flipY = true;
+        }
+    }
+
+    //method for taking damage
     public void TakeDamage(int damageTaken){
         health = health - damageTaken;
         animator.SetBool("Attacked", true);
+        if (health < 0){
+            Destroy(this);
+        }
     }
 }
