e904e673197572ea985b0286adcdda63043cccea
OrbitalCodeRock <37189245+OrbitalCodeRock@users.noreply.github.com>
2023-10-25 01:52:11 -0500

    Updated camera script

--- Assets/Scripts/Prototyping/SimpleCameraTrack.cs
+++ Assets/Scripts/Prototyping/SimpleCameraTrack.cs
@@ -9,6 +9,19 @@ public class SimpleCameraTrack : MonoBehaviour
     private Transform target;
 
     [SerializeField]
+    private float yTarget;
+
+    [SerializeField]
+    float cameraYSpeed;
+
+    // Used for the smoothdamp function
+    private float yDampVelocity = 0.0f;
+    private float smoothTime;
+
+    // What margin between y and target y is acceptable when doing a comparison?
+    private float ydistanceMargin = 0.1f;
+
+    [SerializeField]
     private Boolean useStartingOffset;
 
     [SerializeField]
@@ -35,8 +48,28 @@ public class SimpleCameraTrack : MonoBehaviour
         switch (trackBehaviour)
         {
             case trackingBehaviour.TrackX:
-                this.transform.position = new Vector3(target.position.x + cameraOffset.x, this.transform.position.y, this.transform.position.z);
+                this.transform.position = new Vector3(target.position.x + cameraOffset.x, calculateYPos(), this.transform.position.z);
                 break;
         }
+    }
+
+    public void setYTarget(float target)
+    {
+        this.yTarget = target;
+        smoothTime = Mathf.Abs((this.transform.position.y - yTarget) / cameraYSpeed);
+    }
+
+    // Function that tries to smoothly move the y position of the camera to yTarget.
+    private float calculateYPos()
+    {
+        if(Mathf.Abs(this.transform.position.y - yTarget) < ydistanceMargin)
+        {
+            return Mathf.SmoothDamp(transform.position.y, yTarget, ref yDampVelocity, smoothTime);
+        }
+        else
+        {
+            return yTarget;
+        }
+        
     }
 }
