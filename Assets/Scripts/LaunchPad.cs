50d5244892e9850f5b5cf911c5634f1d20443c5c
PipotronK <dezigurzero@gmail.com>
2023-10-25 21:33:14 -0500

    Turret Code

--- Assets/Scripts/LaunchPad.cs
+++ Assets/Scripts/LaunchPad.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class LaunchPad : MonoBehaviour
 {
-    //Make sure that everything is possible to be edited by the level design team in inspector-c
-    private float bounce = 20f;
+   
+    public float bounce = 20f;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
