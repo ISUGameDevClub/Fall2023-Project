9974d8dec1db8d33f9d1cd8db0c19b191308c8d5
NoMansion <gsam8189@gmail.com>
2023-10-25 21:54:14 -0500

    started work on making goomba explode. Created a script specifically for goomba, and started method to make the explosion work.

---
+++ Assets/Scripts/ShopNodes.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopNodes : MonoBehaviour
+{
+    [SerializeField] boolean revealed;
+    [SerializeField] ArrayList items = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        revealed = false;
+        
+    }
+
+    private boolean isRevealed()
+    {
+        //if player is close, it is revealed. If the player is not, then it is not revealed
+    }
+}
