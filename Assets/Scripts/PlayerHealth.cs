c97741ccca77daf49bd734b9392f25812733ca5b
codydoerr <40671276+codydoerr@users.noreply.github.com>
2023-10-31 01:14:37 -0500

    added ui health and currency

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,16 +6,19 @@ public class PlayerHealth : MonoBehaviour
 {
     
     public int playerHealth;
+    GameObject healthUI;
     [SerializeField] private int playerMaxHealth = 100;
     public delegate void PlayerDeathEventHandler();
     public event PlayerDeathEventHandler OnPlayerDeath;
     // Start is called before the first frame update
     void Start()
     {
+        healthUI = GameObject.Find("Health");
         playerHealth = playerMaxHealth;
     }
 
     public void DamagePlayer(int damagePoint) {
+        healthUI.GetComponent<UIHealth>().ReduceHealth();
         playerHealth -= damagePoint;
         if (playerHealth <= 0) {
             // Kill player Function Here
@@ -24,6 +27,7 @@ public class PlayerHealth : MonoBehaviour
     }
 
     public void HealPlayer(int healPoint) {
+        healthUI.GetComponent<UIHealth>().ReduceHealth();
         playerHealth += healPoint;
         if (playerHealth > playerMaxHealth){
             playerHealth = playerMaxHealth;
