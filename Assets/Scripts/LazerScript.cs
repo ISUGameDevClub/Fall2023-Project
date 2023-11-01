28c6c0cd66e832d5702e0aab4c64270dd6eb3475
Daniel Beyer <dbeyer180@gmail.com>
2023-11-01 11:50:31 -0500

    Lazer Complete?

--- Assets/Scripts/LazerScript.cs
+++ Assets/Scripts/LazerScript.cs
@@ -23,26 +23,22 @@ public class LazerScript : MonoBehaviour
     private Transform emissionTransform;
 
     [SerializeField]
-    private float beamDamage;
+    private int beamDamage;
 
 
     // Ratio for converting beamStart scale to Unity Units.
-    [SerializeField]
+    //[SerializeField]
     private float beamStartUnitRatio = 0.6875f;
 
     // Ratio for converting beamExpanding scale to Unity Units.
-    [SerializeField]
+    //[SerializeField]
     private float beamExpandingUnitRatio = 1f;
 
     private void FixedUpdate()
     {
-        RaycastHit2D[] piercingHits = Physics2D.RaycastAll(emissionTransform.position, this.transform.up, maxLazerRange, piercingLayers.value);
-        for(int i = 0; i < piercingHits.Length; i++)
-        {
-            // Look for and trigger damage methods on suitable objects
-        }
+        float piercingCastRange = maxLazerRange;
 
-
+        // Raycast to find a blocking object
         RaycastHit2D blockingHit = Physics2D.Raycast(emissionTransform.position, this.transform.up, maxLazerRange, blockingLayers.value);
         // If nothing was hit to block the lazer.
         if (!blockingHit)
@@ -50,21 +46,41 @@ public class LazerScript : MonoBehaviour
             beamStart.size = new Vector2(beamStart.size.x, 1);
             beamExpanding.size = new Vector2(beamExpanding.size.x, maxLazerRange);
         }
-        else if(blockingHit.distance <= beamStartUnitRatio)
-        {
-            beamStart.size = new Vector2(beamStart.size.x, blockingHit.distance);
-            beamExpanding.size = new Vector2(beamExpanding.size.x, 0);
-            // Trigger damage method on hit object if suitable
-        }
-        else
-        {
-            beamStart.size = new Vector2(beamStart.size.x, 1);
+        else{
+            // We don't want our lazer to pierce through a blocking object.
+            piercingCastRange = blockingHit.distance;
 
-            // The distance we need to cover with the expanding portion of the lazer beam
-            // float distanceToCover = blockingHit.distance - beamStartUnitRatio;
-            float distanceToCover = blockingHit.distance - 0.61f;
-            beamExpanding.size = new Vector2(beamExpanding.size.x, distanceToCover * beamExpandingUnitRatio);
+            // Trigger damage method on hit object if suitable
+            PlayerHealth playerHealth = blockingHit.collider.GetComponent<PlayerHealth>();
+            if(playerHealth){
+                playerHealth.DamagePlayer(beamDamage, blockingHit.point);
+            }
+
+            // Update lazer beam visuals
+            if(blockingHit.distance <= beamStartUnitRatio){
+                beamStart.size = new Vector2(beamStart.size.x, blockingHit.distance);
+                beamExpanding.size = new Vector2(beamExpanding.size.x, 0);
+            }
+            else{
+                beamStart.size = new Vector2(beamStart.size.x, 1);
+                // The distance we need to cover with the expanding portion of the lazer beam
+                // float distanceToCover = blockingHit.distance - beamStartUnitRatio;
+                float distanceToCover = blockingHit.distance - 0.61f;
+                beamExpanding.size = new Vector2(beamExpanding.size.x, distanceToCover * beamExpandingUnitRatio);
+            }
         }
+
+        // Raycast to find objects that can be pierced.
+        RaycastHit2D[] piercingHits = Physics2D.RaycastAll(emissionTransform.position, this.transform.up, piercingCastRange, piercingLayers.value);
+        for(int i = 0; i < piercingHits.Length; i++)
+        {
+            // Look for and trigger damage methods on suitable objects
+            PlayerHealth playerHealth = piercingHits[i].collider.GetComponent<PlayerHealth>();
+            if(playerHealth){
+                playerHealth.DamagePlayer(beamDamage, piercingHits[i].point);
+            }
+        }
+
     }
 
 }
