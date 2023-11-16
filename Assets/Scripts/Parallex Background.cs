9f0230f32faf078ec2ccad1743c609e9d5c120dd
PipotronK <dezigurzero@gmail.com>
2023-11-15 20:22:32 -0600

    Update Parallex Background.cs

--- Assets/Scripts/Parallex Background.cs
+++ Assets/Scripts/Parallex Background.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ParallexBackground : MonoBehaviour
 {
+    [SerializeField] private float parallexEffectMultiplier = 0.5f;
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
     private void Start()
@@ -12,8 +13,11 @@ public class ParallexBackground : MonoBehaviour
         lastCameraPosition = cameraTransform.position;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 deltaMovement = cameraTransform.position = lastCameraPosition;
+        float parallexEffectMultiplier = 0.5f;
+        transform.position += deltaMovement * parallexEffectMultiplier;
+        lastCameraPosition = cameraTransform.position;
     }
 }
