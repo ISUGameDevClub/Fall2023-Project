210d084798042d1e56441e9a7499a5de846fc6a5
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-12-04 21:51:42 -0600

    Credits

--- Assets/Scripts/ScrollAnim.cs
+++ Assets/Scripts/ScrollAnim.cs
@@ -2,44 +2,9 @@ using UnityEngine;
 
 public class ScrollAnim : MonoBehaviour
 {
-    Transform scrollTransform;
-    [SerializeField] float scrollSpeed;
-    [SerializeField] float stopPoint;
-    [SerializeField] float endTimer;
-    public bool creditsComplete;
-
-    [SerializeField] string yPosition; //for debug purposes
-    public LevelLoader LoadScript;
-
-    // Start is called before the first frame update
-    void Start()
+    public void AutoBackToMainMenu()
     {
-        creditsComplete = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        yPosition = transform.position.y.ToString(); //debug to find the y position, didn't allign with the object's transform tag
-        if (transform.position.y > stopPoint)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - (Time.deltaTime * scrollSpeed));           
-        }
-        else
-        {
-            creditsComplete = true;
-        }
-        
-        if (creditsComplete == true)
-        {
-            if (endTimer > 0)
-            {
-                endTimer -= Time.deltaTime;
-            }
-            else
-            {
-                LoadScript.StartTransition("");
-            }
-        }
+        FindObjectOfType<LevelLoader>().StartTransition("0MainMenu");
+        FindObjectOfType<UISFX>().ButtonSelectSFX();
     }
 }
