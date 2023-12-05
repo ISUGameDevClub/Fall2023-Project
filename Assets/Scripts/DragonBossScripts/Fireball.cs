2e9a4b62f46fa09b423d4ebf8e12cf54f5f3cde5
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2023-12-05 02:08:45 -0600

    DragonBoss fixes

--- Assets/Scripts/DragonBossScripts/Fireball.cs
+++ Assets/Scripts/DragonBossScripts/Fireball.cs
@@ -12,8 +12,13 @@ public class Fireball : MonoBehaviour
     {
         player = GameObject.FindObjectOfType<PlayerMovement>().gameObject;
         rb = GetComponent<Rigidbody2D>();
+
         Vector2 initialDir = player.transform.position - this.transform.position;
         rb.velocity = initialDir * speed;
+
+        float angle = Mathf.Atan2(initialDir.y, initialDir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, 0, angle + 180f);
+
         Destroy(gameObject, destroyTime);
     }
 }
